db88dbe06f7c0be76fc4bfeac57ddab954520389
Hussein Aboelseoud <hussein.aboelseoud@gmail.com>
2016-09-17 21:21:35 +0200

    Only use POST and GET

--- routes/api.php
+++ routes/api.php
@@ -18,11 +18,11 @@ Route::get('/products', 'ProductsController@index')->middleware('api');
 Route::get('/users/{id}/children', 'UsersController@getChildren')->middleware('api');
 Route::get('/children/{id}', 'ChildrenController@get')->middleware('api');
 Route::post('/users/{id}/children', 'UsersController@addChild')->middleware('api');
-Route::delete('/children/{id}', 'ChildrenController@destroy')->middleware('api');
-Route::put('/children/{id}', 'ChildrenController@update')->middleware('api');
+Route::get('/children/{id}', 'ChildrenController@destroy')->middleware('api');
+Route::post('/children/{id}', 'ChildrenController@update')->middleware('api');
 Route::get('/children/{id}/allowed_products', 'ChildrenController@allowedProducts')->middleware('api');
 Route::get('/children/{cid}/add_to_blacklist/{pid}', 'ChildrenController@addToBlacklist')->middleware('api');
-Route::delete('/children/{cid}/remove_from_blacklist/{pid}', 'ChildrenController@removeFromBlacklist')->middleware('api');
+Route::get('/children/{cid}/remove_from_blacklist/{pid}', 'ChildrenController@removeFromBlacklist')->middleware('api');
 Route::get('/children/{cid}/buy/{pid}', 'ChildrenController@buy')->middleware('api');
 Route::get('/children/{cid}/history', 'ChildrenController@history')->middleware('api');
 Route::get('/children/{cid}/credit', 'ChildrenController@credit')->middleware('api');
@@ -31,7 +31,7 @@ Route::get('/children/{cid}/credit_used_this_month', 'ChildrenController@creditU
 Route::post('/users/{id}/cards', 'UsersController@addCard')->middleware('api');
 Route::get('/users/{id}/cards', 'UsersController@getCards')->middleware('api');
 Route::get('/cards/{id}', 'CardsController@get')->middleware('api');
-Route::delete('/cards/{id}', 'CardsController@destroy')->middleware('api');
+Route::get('/cards/{id}', 'CardsController@destroy')->middleware('api');
 
 
 
